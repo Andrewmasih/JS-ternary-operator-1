d1a3f0bba6b8f3379689bfd06c6d13089ac8d96e
Andrew Masih <armasih@outlook.com>
2021-05-07 16:22:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* its a one line shorthand for 'if' 'else' statement, it check wheater if the condition is true or false, you just need one line code: conditon? expression1: expresson2 */
+
+
+
+let age = 25;
+
+(age > 18) ?  "You are soooo mature": "You are still a child"
+
+/* if the ternary is true then expression1 will run, otherwise expression2, this is the same as: 'if' (condition) {expression1} 'esle' {expression2}   */
+
+// The code below is the same logic but written longhand
+if (age > 18) {
+  "You are soooo mature"
+} else {
+  "You are still a child"
+}

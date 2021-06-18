2b39301a413b7386b0447c7bd2ea76c1e8ef08d9
Andrew Masih <armasih@outlook.com>
2021-06-18 14:22:39 +0000

    added commit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 
 let age = 25;
 
-(age > 18) ?  "You are soooo mature": "You are still a child"
+(age > 18) ?  "You are old enough to drive a car": "You are not old enough to drive a car"
 
 /* if the ternary is true then expression1 will run, otherwise expression2, this is the same as: 'if' (condition) {expression1} 'esle' {expression2}   */
 
 // The code below is the same logic but written longhand
 if (age > 18) {
-  "You are soooo mature"
+  "You are old enough to drive a car"
 } else {
-  "You are still a child"
+  "You are not old enough to drive a car"
 }
